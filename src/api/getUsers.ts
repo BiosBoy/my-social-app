2d49e36a15e28891f992f181cefd16dca8202a1f
Sviat Kuzhelev <30953392+BiosBoy@users.noreply.github.com>
2024-05-16 12:30:32 +0200

    minore fixes

--- src/api/getUsers.ts
+++ src/api/getUsers.ts
@@ -3,8 +3,6 @@ import { User } from "@/interfaces/data";
 const getUsers = (): User[] => {
   const users = localStorage.getItem("users");
 
-  console.log(users, "TST");
-
   if (!users || users === "undefined") {
     return [];
   }
