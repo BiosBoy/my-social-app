f481f41038991ce37e2b92fcf1642f84b6b5490b
Sviat Kuzhelev <30953392+BiosBoy@users.noreply.github.com>
2024-05-16 11:15:34 +0200

    added logo

--- src/components/AddPostModal.tsx
+++ src/components/AddPostModal.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FormEvent, useState } from "react";
 import getPosts from "../utils/getPosts";
 import { useAuth } from "@/auth/AuthContext";
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,49 @@
+"use client";
 import Link from "next/link";
 import { useAuth } from "@/auth/AuthContext";
+import { useState } from "react";
+import Image from "next/image";
 
 const NavBar = () => {
   const { currentUser } = useAuth();
 
-  console.log(currentUser?.name, "currentUser?.name");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav>
-      {currentUser?.name ? (
-        <>
-          <Link href="/">Home</Link>
-          <Link href="/feed">My feed</Link>
-          <Link href="/friends">My friends</Link>
-          <Link href={`/${currentUser?.name}`}>My profile</Link>
-          <Link href="/signout">Sign out</Link>
-        </>
-      ) : (
-        <>
-          <Link href="/">Home</Link>
-          <Link href="/signin">Sign in</Link>
-          <Link href="/signup">Sign up</Link>
-        </>
-      )}
-    </nav>
+    <div className="navMenu">
+      <div className="logoWrap">
+        <Image
+          alt="logo"
+          src={require("../static/images/logo.svg")}
+          width={75}
+          height={20}
+        />
+      </div>
+      <button
+        className="burgerMenu"
+        aria-label={`mobile menu ${isMenuOpen}`}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        Menu
+      </button>
+      <nav className={(isMenuOpen && " open") || ""}>
+        {currentUser?.name ? (
+          <>
+            <Link href="/">Home</Link>
+            <Link href="/feed">My feed</Link>
+            <Link href="/friends">My friends</Link>
+            <Link href={`/${currentUser?.name}`}>My profile</Link>
+            <Link href="/signout">Sign out</Link>
+          </>
+        ) : (
+          <>
+            <Link href="/">Home</Link>
+            <Link href="/signin">Sign in</Link>
+            <Link href="/signup">Sign up</Link>
+          </>
+        )}
+      </nav>
+    </div>
   );
 };
 
